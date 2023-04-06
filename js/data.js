72b8f872ecec856be543d833080edca3b22c309d
altankurt <aaltankurt@gmail.com>
2023-04-06 15:34:36 +0300

    feat: Fetch tweets from data.js file

---
+++ js/data.js
@@ -0,0 +1,29 @@
+export const tweets = [
+  {
+    id: 0,
+    profilePicture: "assets/images/user-profil.png",
+    name: "Altan Kurt",
+    username: "@aaltankurt",
+    date: "1h",
+    text: "I had gone out to take photos early in the morning, and it was my lucky day!",
+    image: "assets/images/tw-pic-2.png",
+  },
+  {
+    id: 1,
+    profilePicture: "assets/images/tw-github.png",
+    name: "GitHub",
+    username: "@github",
+    date: "2h",
+    text: "How are you protecting your organization from security threats? Join us at #OfficialCyberSummit to learn how GitHub can help.",
+    image: "assets/images/tw-pic-1.png",
+  },
+  {
+    id: 2,
+    profilePicture: "assets/images/tw-deno.png",
+    name: "Deno",
+    username: "@deno_land",
+    date: "4d",
+    text: "Got a question for one of our engineers? Come ask it at our next Office Hours — tomorrow, Friday 9am PT, on our Discord! RVSP -> discord.gg/deno",
+    image: "",
+  },
+];
