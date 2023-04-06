0fec28dc198edafb67af0784ba598960c976b4c9
altankurt <aaltankurt@gmail.com>
2023-04-06 19:14:06 +0300

    fix: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./js/app.js"],
   theme: {
     extend: {
       fontFamily: {
